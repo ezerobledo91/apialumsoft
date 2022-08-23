7613c5286693e146d186dee6b48e63b81c203576
Marcelo Robledo <ezer.robledo@gmail.com>
2022-08-22 23:55:20 -0300

    Correction Model Vidrio

--- models/Vidrio.js
+++ models/Vidrio.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose')
 const VidrioSchema = new mongoose.Schema(
     {
         nombre: { type: String, required: true },
-        categoria: { type: Array, required: true },
+        categoria: { type: String, required: true },
         espesor: { type: Number,required:true },
         proveedor: { type: String },
+        costo: { type: Number,required:true },
         precio: { type: Number,required:true },
+        iva: {type: String},
+        porcentaje: {type:Number}
+
     }
 )
 
